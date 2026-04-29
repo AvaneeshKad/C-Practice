057d52bc03b84216b9694d56cd04d763f291af9f
Avaneesh Kad <avaneeshkad@gmail.com>
2026-04-29 02:19:51 -0400

    This is a practice code snippet for iterator data type

---
+++ iterator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    set<int> mySet = { 0, 10, 20, 30, 40, 50, 60, 70, 80, 90 };
+    auto itStart = mySet.lower_bound(5);
+    auto itEnd = mySet.upper_bound(30);
+    mySet.erase(itStart, itEnd);
+    cout << *itEnd << "\n";
+    auto zeroFind = mySet.find(0);
+    cout << &zeroFind << "\n";
+    return 0;
+}
