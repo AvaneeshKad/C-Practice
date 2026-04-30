cef8510efd91fdbc62c805e12babcc019966f7c3
Avaneesh <avaneeshkad@gmail.com>
2026-04-30 04:38:51 +0000

    learnt do-while loop hands-on!

--- loop.cpp
+++ loop.cpp
@@ -7,5 +7,14 @@ int main() {
         cout<<"Iteration : "<<i<<"\n";
         i++;
     }
+
+
+    // differet loop : do while
+
+    int x = 10;
+
+    do {
+        cout<<"This will print once even though 10 is not less than 5."<<"\n";
+    } while(x < 5);
     return 0;
 }
