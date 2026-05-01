08e60616629678a3460f077b29920d46f097575d
Avaneesh <avaneeshkad@gmail.com>
2026-05-01 02:18:31 +0000

    learning the difference between size() and capacity()

--- vectors.cpp
+++ vectors.cpp
@@ -7,6 +7,8 @@ int main() {
     vector<int> x = {10, 20 ,30 ,40 ,50 ,60};
 
     x.push_back(90);
-    return 0;
+    cout <<"The capacity this vector is taking is : "<< x.capacity()<<"\n";
+    cout <<"The size this vector is : "<< x.size()<<"\n";
 
+    return 0;
 }
