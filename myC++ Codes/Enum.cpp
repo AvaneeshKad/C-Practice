277ef3b4b7cc94510ff32ea73c27c70d4feac1d1
AvaneeshKad <avaneeshkad@gmail.com>
2026-05-09 22:55:35 -0400

    Learning Enum with some basic examples

---
+++ myC++ Codes/Enum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+enum Example {
+    A, B, C
+};
+
+int a = 0;
+int b = 1;
+int c = 2;
+
+int main() {
+    Example value = B;
+    if (value == 1) {
+        cout<<"Thats correct"<<"\n";
+    }
+}
