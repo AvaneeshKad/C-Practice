fa62fd81df63ef1c6d76464574caf0643aa1b061
Avaneesh <avaneeshkad@gmail.com>
2026-05-09 22:30:59 -0400

    Fix comments formatting in RecursiveSubtract function

--- recursion.cpp
+++ recursion.cpp
@@ -3,14 +3,12 @@ using namespace std;
 
 // This function calls itself until Numbers reaches 0
 void RecursiveSubtract(int Numbers) {
-    // 1. Base Case: Stop when we hit 0
+    // 1) Base Case: Stop when we hit 0
     if (Numbers < 1) {
         return; 
     }
-
     // 2. Action: Print the current number
     cout << Numbers << " ";
-
     // 3. Recursive Step: Call the function again with (Numbers - 1)
     RecursiveSubtract(Numbers - 1);
 }
@@ -19,10 +17,8 @@ int main() {
     int n;
     cout << "Enter a starting number: ";
     cin >> n;
-
     cout << "Counting down: ";
     RecursiveSubtract(n);
     cout<<endl;
-    
     return 0;
 }
